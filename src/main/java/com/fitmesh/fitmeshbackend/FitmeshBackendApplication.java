8c14cc2d39c2b1e78a4ca79dfded5f5bbb505a5e
0nyinye <onyinyeekezi2017@gmail.com>
2026-01-21 11:44:54 +0100

    initial project setup

---
+++ src/main/java/com/fitmesh/fitmeshbackend/FitmeshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitmesh.fitmeshbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeshBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitmeshBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitmesh/fitmeshbackend/FitmeshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmesh.fitmeshbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmeshBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
